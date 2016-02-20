c178ca91a96b504a7aede52352cb138d657d112b
chickensmitten <chia.shengyeong@gmail.com>
2016-02-20 14:11:00 +0800

    changes to ip

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,21 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'ben-dixon'
+set :application, 'bendixon'
 set :deploy_user, 'deploy'
 set :scm, :git
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:chickensmitten/contactbooks2.git'
+
+set :rbenv_type, :system
+set :rbenv_ruby, '2.1.2'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w(rake gem bundle ruby rails)
+
+set :keep_releases, 5
+
+set :linked_files, %w{config/database.yml config/secrets.yml}
+
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
@@ -38,23 +49,5 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
 
 end
